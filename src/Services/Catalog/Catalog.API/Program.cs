f2a037aa22c3179dfe3e3966aab7882b01d73af1
Hà Đỗ <haproadg@gmail.com>
2023-04-24 23:59:34 +0700

    feat: docker file, docker compose

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,6 @@
 using Catalog.API.Data;
 using Catalog.API.Repository;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,17 +9,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c 
+    => c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1"}));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ICatalogContext, CatalogContext>();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "_MyAllowSpecificOrigins",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://example.com",
+                                              "http://www.contoso.com");
+                      });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("_MyAllowSpecificOrigins");
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
 }
 
 app.UseHttpsRedirection();
