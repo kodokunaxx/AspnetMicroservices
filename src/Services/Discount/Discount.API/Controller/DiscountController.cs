a59432eb04dde25c1cdece8ba484d44768f7be8f
Hà Đỗ <haproadg@gmail.com>
2023-12-27 11:16:36 +0700

    feat: add docker file

---
+++ src/Services/Discount/Discount.API/Controller/DiscountController.cs
@@ -0,0 +1,77 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Discount.API.Controller
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        #region Declare
+        private readonly IDiscountRepository _repository;
+        #endregion
+
+        #region Constructor
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        #endregion
+
+        #region Method
+        /// <summary>
+        /// Lấy thông tin giảm giá
+        /// </summary>
+        /// <param name="productName"></param>
+        /// <returns></returns>
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await _repository.GetDiscount(productName);
+            return Ok(coupon);
+        }
+
+        /// <summary>
+        /// Sửa giảm giá
+        /// </summary>
+        /// <param name="coupon">coupon</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            var res = await _repository.CreateDiscount(coupon);
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        /// <summary>
+        /// Sửa giảm giá
+        /// </summary>
+        /// <param name="coupon">coupon</param>
+        /// <returns></returns>
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon Discount)
+        {
+            var res = await _repository.UpdateDiscount(Discount);
+            return Ok(res);
+        }
+
+        /// <summary>
+        /// Xóa giảm giá
+        /// </summary>
+        /// <param name="product">product</param>
+        /// <returns></returns>
+        [HttpDelete("{productName}", Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteDiscount(string productName)
+        {
+            await _repository.DeleteDiscount(productName);
+            return Ok();
+        }
+        #endregion
+    }
+}
--- src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -14,6 +14,11 @@ namespace Discount.API.Repositories
             _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
             _connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
         }
+        /// <summary>
+        /// Lấy thông tin giảm giá
+        /// </summary>
+        /// <param name="productName">Tên sp</param>
+        /// <returns></returns>
         public async Task<Coupon> GetDiscount(string productName)
         {
             var coupon = await _connection.QueryFirstOrDefaultAsync<Coupon>("SELECT * FROM Coupon where ProductName = @ProductName", new { ProductName = productName });
@@ -23,24 +28,41 @@ namespace Discount.API.Repositories
             }
             return coupon;
         }
+        /// <summary>
+        /// Tạo giảm giá
+        /// </summary>
+        /// <param name="coupon">phiếu mua hàng</param>
+        /// <returns></returns>
         public async Task<bool> CreateDiscount(Coupon coupon)
         {
             var res = await _connection.ExecuteAsync("INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)", coupon);
             return res == 0 ? false : true;        
         }
 
+        /// <summary>
+        /// Cập nhật giảm giá
+        /// </summary>
+        /// <param name="coupon">phiếu mua hàng</param>
+        /// <returns></returns>
         public async Task<bool> UpdateDiscount(Coupon coupon)
         {
             var res = await _connection.ExecuteAsync("UPDATE Coupon SET ProductName = @ProductName, Description = @Description, Amount = @Amount WHERE Id = @Id", coupon);
             return res == 0 ? false : true;
         }
 
+        /// <summary>
+        /// Xóa giảm giá
+        /// </summary>
+        /// <param name="productName">Tên sp</param>
+        /// <returns></returns>
         public async Task<bool> DeleteDiscount(string productName)
         {
             var res = await _connection.ExecuteAsync("DELETE Coupon WHERE ProductName = @ProductName", new {ProductName = productName});
             return res == 0 ? false : true;
         }
-
+        /// <summary>
+        /// Đóng connection
+        /// </summary>
         public void Dispose()
         {
             if (_connection.State == ConnectionState.Open)
